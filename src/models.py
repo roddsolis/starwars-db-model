d286826396dc9e0f0a7aef2dac9a7e65daa994c4
Rodrigo Solis <72800496+roddsolis@users.noreply.github.com>
2023-12-19 19:15:50 +0000

    modelo de datos starwars

--- src/models.py
+++ src/models.py
@@ -1,32 +1,69 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(200), nullable=False)
+    password = Column(String(200),  nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    # Relación uno a muchos con Planets, Characters y Vehicles
+    planets = relationship('Favorites', backref='user')
+    characters = relationship('Favorites', backref='user')
+    vehicles = relationship('Favorites', backref='user')
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    hair_color = Column(String, nullable=False)
+    birth_year = Column(String, nullable=False)
 
-    def to_dict(self):
-        return {}
+    # Relación uno a muchos con Favorites
+    favorites = relationship('Favorites', backref='character')
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    population = Column(String, nullable=False)
+    terrain = Column(String, nullable=False)
+
+    # Relación uno a muchos con Favorites
+    favorites = relationship('Favorites', backref='planet')
+
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    manufacturer = Column(String, nullable=False)
+    passengers = Column(String, nullable=False)
+
+    # Relación uno a muchos con Favorites
+    favorites = relationship('Favorites', backref='vehicle')
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+
+    # Relaciones muchos a uno con User, Planets, Characters y Vehicles
+    user = relationship('User', backref='planets')
+    planet = relationship('Planets', backref='favorites')
+    character = relationship('Characters', backref='favorites')
+    vehicle = relationship('Vehicles', backref='favorites')
+
+
+def to_dict(self):
+    return {}
